a2dd38dc4c3e7cc22875afaa95a0d2f63d375f99
JP Mondal <jpmondal2003@gmail.com>
2025-06-16 20:27:07 +0600

    02 : customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+
 export default function Home() {
-  return(
-    <div>
-<Button variant="destructive">Hello World</Button>
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>iam a button</Button>
+        </div>
+        <div>
+          <Input placeholder="i am an input"/>
+        </div>
+        <div>
+          <Progress value={50}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder="i am a textarea"></Textarea>
+        </div>
+        <div>
+          <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
